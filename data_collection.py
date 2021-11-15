4c1720d9972767395ab5caea34758efbe166bf1d
Etienne Guillet <guilletetienne.pro@gmail.com>
2021-11-15 11:16:18 +0900

    print player infos

--- data_collection.py
+++ data_collection.py
@@ -3,9 +3,11 @@ import pandas as pd
 
 api_key = "RGAPI-c5e744ba-2a89-424b-a66a-807fea83c360"
 
+challenger = 'https://euw1.api.riotgames.com/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key=' + api_key
 grandmaster = 'https://euw1.api.riotgames.com/lol/league/v4/grandmasterleagues/by-queue/RANKED_SOLO_5x5?api_key=' + api_key
+master = 'https://euw1.api.riotgames.com/lol/league/v4/masterleagues/by-queue/RANKED_SOLO_5x5?api_key=' + api_key
 
-data = requests.get(grandmaster)
+data = requests.get(challenger)
 league_df = pd.DataFrame(data.json())
 
 league_df.reset_index(inplace=True)
@@ -15,12 +17,13 @@ league_df = league_df.drop(['index', 'queue', 'name', 'leagueId', 'entries', 'ra
 league_df.info()
 
 for i in range(len(league_df)):
+    print(i, len(league_df))
     # if (i > 3): 
     #     break
     try:
         request_url = 'https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/' + league_df['summonerId'].iloc[i] + '?api_key=' + api_key 
-        print(request_url)
         r = requests.get(request_url)
+        print(r.json())
             
         while r.status_code == 429:
             request_url = 'https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/' + league_df['summonerId'].iloc[i] + '?api_key=' + api_key 
